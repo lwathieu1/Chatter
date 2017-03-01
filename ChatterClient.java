685286dce7912bd8bc83db763a72c2e168d8a88c
lwathieu1 <lbw29@georgetown.edu>
2017-03-01 13:43:26 -0500

    Created Files

--- ChatterClient.java
+++ ChatterClient.java
@@ -1,5 +1,5 @@
 package Chatter;
 
-public class ChatterClient {
+public class ChatterClient{
 
 }
--- ChatterServer.java
+++ ChatterServer.java
@@ -1,5 +1,5 @@
 package Chatter;
 
-public class ChatterServer {
+public class ChatterServer{
 
 }
