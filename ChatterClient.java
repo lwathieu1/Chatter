d6a1ae4998a8e18abe86b282845dae844d4496aa
lwathieu1 <lbw29@georgetown.edu>
2017-03-14 16:00:35 -0400

    Finished almost everything, we just need to figure out how to quit

--- ChatterClient.java
+++ ChatterClient.java
@@ -1,6 +1,114 @@
 package Chatter;
 
-//jboy was here
-public class ChatterClient{
+import java.io.*;
+import java.net.Socket;
+import java.util.*;
 
+public class ChatterClient
+{
+	ClientListener listener;
+	private Scanner scanner;
+	//String nickName;
+	
+	BufferedWriter bout;
+
+	public static void main( String[] args )
+	{	
+		if (args.length == 2) 
+		{
+			new ChatterClient(args[0], Integer.parseInt(args[1]));
+        }
+		else
+		{
+			System.out.println("Use format: java ChatterClient <host name> <port number>");
+            System.out.println("Using default hostname \"127.0.0.1\" and port 11123 ");
+            new ChatterClient("127.0.0.1", 11123);
+		}
+	}
+
+	
+	public ChatterClient(String name, int port)
+	{		
+		try
+		{
+	        Socket sock = new Socket(name, port);
+	         
+	        OutputStream out = sock.getOutputStream();
+		    bout = new BufferedWriter( new OutputStreamWriter( out ) );
+		    
+	 		scanner = new Scanner(System.in);
+			System.out.println("Welcome! What is your nickname (no spaces)?");
+			String nickName = getUserInput();
+			//System.out.println("Hi "+ nickName +"! DIRECTIONS HERE");
+			write("/nick " + nickName);
+		     
+	        listener = new ClientListener(sock);
+	        listener.start();
+	         
+	        while(true)
+	        {
+	        	String userCommand = getUserInput();
+	        	write(userCommand);
+	        	if (userCommand.split(" ")[0].equals("/quit"))
+	        	{
+	        		System.out.println("quit somehow!");
+	        		//What to do to quit??
+	        	}
+	        			
+	        }
+				
+	    }
+	    catch ( Exception e )
+	     { System.err.println(e); }
+	}
+	
+	
+	private String getUserInput()
+	{
+		String line = scanner.nextLine();
+		return line;
+	}
+	
+	public void write(String s) throws IOException 
+	{
+        bout.write(s + "\n");
+        bout.flush();
+	}
+	
+	public class ClientListener extends Thread
+	{
+		//private Socket serverSocket;
+		BufferedReader bin;
+		
+		
+		public ClientListener(Socket s) throws Exception
+		{
+			//serverSocket = s;
+			
+	        InputStream in = s.getInputStream();
+	        bin = new BufferedReader( new InputStreamReader(in) );
+		}
+		
+		public String readLine() throws IOException
+		{
+	         String line = bin.readLine();
+	         return line;
+		}
+		
+		@Override
+	    public void run()
+	    {
+			try
+			{
+				while(true)
+				{
+					String serverLine = readLine();
+					System.out.println(serverLine);
+				}
+			}
+			catch (Exception e) 
+			{e.printStackTrace();}
+	    }
+		
+	}
 }
--- ChatterServer.java
+++ ChatterServer.java
@@ -1,6 +1,164 @@
 package Chatter;
 
-//lmoney was here
-public class ChatterServer{
+import java.io.*;
+import java.net.*;
+import java.util.Iterator;
+import java.util.LinkedList;
 
+
+
+public class ChatterServer
+{
+	ServerSocket sock;
+	LinkedList<ServerListener> listOfListeners = new LinkedList<ServerListener>();
+	
+	public static void main( String[] args )
+	{
+		int portNum = 11123;
+		if (args.length == 1)
+			portNum = Integer.parseInt(args[0]);
+		else
+		{
+			System.out.println("Please run with the following format:");
+			System.out.println("java ChatterServer <port number>");
+			System.out.println("In the meantime, using default port number of 11123");
+		}
+
+		new ChatterServer(portNum);
+	}
+	
+	public ChatterServer(int port)
+	{
+		try
+		{
+	        sock = new ServerSocket(port); // open socket
+	        answerThePhone();
+		}
+	    catch( Exception e ) { System.err.println("DateServer: error = "+e); } 
+	}
+	
+	
+	private void answerThePhone() throws Exception 
+	{
+		while (true) // has no way to stop as written
+        {
+			System.out.println("Listening on port "+sock.getLocalPort()+"...");
+			// when client calls, establish output stream to client and send date
+	    	// does not return until client calls up
+	        Socket client = sock.accept(); // this blocks code until a client calls      
+	        System.out.println("DateServer: accepts client connection! ");
+	        ServerListener sListenerThread = new ServerListener(client);
+	        listOfListeners.add(sListenerThread);
+	        sListenerThread.start();
+        }
+
+	}
+	
+	
+	private void tellOthers(String message) throws Exception 
+	{
+		System.out.println("trying to tell others!!");
+		Iterator <ServerListener> it = listOfListeners.iterator();
+	 	while( it.hasNext() )
+	 	{
+	 		ServerListener listener = it.next(); 
+	 		listener.write(message);
+	 	}
+	}
+	
+	private void tellOne(String message, String name) throws Exception
+	{
+		System.out.println("trying to tell "+ name);
+		Iterator <ServerListener> it = listOfListeners.iterator();
+	 	while( it.hasNext() )
+	 	{
+	 		ServerListener listener = it.next(); 
+	 		if (listener.nickName.equals(name))
+	 			listener.write(message);
+	 	}
+	}
+	
+	
+	public class ServerListener extends Thread
+	{
+		public String nickName = "defaultName";
+		private Socket clientSocket;
+		BufferedReader bin;
+		BufferedWriter bout;
+		//PrintWriter pout;
+		
+		public ServerListener(Socket s) throws Exception
+		{
+			clientSocket = s;
+			
+			InputStream in = clientSocket.getInputStream();
+			bin = new BufferedReader( new InputStreamReader(in) );
+			
+			OutputStream out = clientSocket.getOutputStream();
+		    bout = new BufferedWriter( new OutputStreamWriter( out ) );
+			
+			//pout = new PrintWriter( clientSocket.getOutputStream(), true);
+			
+		}
+		
+		
+		public void write(String s) throws IOException 
+		{
+			bout.write(s+"\n");
+	        bout.flush();
+		}
+		
+		public String readLine() throws IOException
+		{
+            // Thread.sleep(1000);
+            String msg = bin.readLine();
+            return msg;
+		}
+		
+		@Override
+	    public void run()
+	    {
+			try {
+				while(true)
+				{
+					String inLine = readLine();
+					System.out.println("Got something from the user!");
+					if (!inLine.isEmpty())
+					{
+						System.out.println("The Line from "+ nickName +" is = ");
+						System.out.println(inLine);
+						
+						//Process the incoming string
+						String[] arrOfWords = inLine.split(" ");
+						if (arrOfWords[0].equals("/nick") && arrOfWords.length > 1)
+						{
+							String oldNickName = nickName;
+							nickName = arrOfWords[1];
+							if (oldNickName.equals("defaultName"))
+								tellOthers(nickName+" joined the Chat Room!");
+							else
+								tellOthers(oldNickName + " changed their name to " + nickName);
+						}
+						else if (arrOfWords[0].equals("/dm") && arrOfWords.length > 1)
+						{
+							String ya = "DM from "+nickName+ ": "+ inLine.split(" ", 2)[1].split(" ", 2)[1];
+							tellOne(ya, arrOfWords[1]);
+						}
+						else if (arrOfWords[0].equals("/quit"))
+						{
+							tellOthers(nickName + " has left the chat room!");
+							//WHAT TO DO HERE?
+						}
+						else tellOthers(nickName+": "+inLine);
+
+						
+					}
+				}
+			}
+			catch (Exception e) 
+				{e.printStackTrace();}
+	    }
+	}
+	
+	
 }
